edb7551314e4897969b4754a24ab0fc02b743a3a
xiexianbin <a.b@hotmail.com>
2014-11-09 20:57:13 +0800

    兼容阿里云oss配置

--- ueditor/src/main/java/com/baidu/ueditor/hunter/FileManager.java
+++ ueditor/src/main/java/com/baidu/ueditor/hunter/FileManager.java
@@ -1,18 +1,27 @@
 package com.baidu.ueditor.hunter;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
+import com.aliyun.openservices.oss.OSSClient;
+import com.aliyun.openservices.oss.model.Bucket;
 import com.baidu.ueditor.PathFormat;
 import com.baidu.ueditor.define.AppInfo;
 import com.baidu.ueditor.define.BaseState;
 import com.baidu.ueditor.define.MultiState;
 import com.baidu.ueditor.define.State;
+import com.qikemi.packages.alibaba.aliyun.oss.BucketService;
+import com.qikemi.packages.alibaba.aliyun.oss.OSSClientFactory;
+import com.qikemi.packages.alibaba.aliyun.oss.ObjectService;
 import com.qikemi.packages.alibaba.aliyun.oss.properties.OSSClientProperties;
+import com.qikemi.packages.utils.SystemUtil;
 
 public class FileManager {
 
@@ -39,8 +48,24 @@ public class FileManager {
 		
 		if(OSSClientProperties.useStatus){
 			// 从阿里云OSS服务器中获取文件 
+			// 获取prefix
+			// dir = G:/tomcat/apache-tomcat-7.0.56/wtpwebapps/ueditor//upload/image/
+			// projectrootpath = G:/tomcat/apache-tomcat-7.0.56/wtpwebapps/ueditor/
+			String prefix = this.dir.replace(SystemUtil.getProjectRootPath(), "").replaceFirst("/", "");
 			// 获取路径 
+			OSSClient client = OSSClientFactory.createOSSClient();
+			List<String> objectList = ObjectService.listObject(client, OSSClientProperties.bucketName, null, prefix);
+
+			if ( index < 0 || index > objectList.size() ) {
+				state = new MultiState( true );
+			} else {
+				Object[] fileList = Arrays.copyOfRange( objectList.toArray(), index, index + this.count );
+				state = this.getOSSState( fileList );
+			}
 			
+			state.putInfo( "start", index );
+			state.putInfo( "total", objectList.size() );
+			// 处理连接问题 
 			
 		}else{
 			// 从文件夹中获取文件 
@@ -53,21 +78,23 @@ public class FileManager {
 			}
 			
 			list = FileUtils.listFiles( dir, this.allowFiles, true );
+			
+			if ( index < 0 || index > list.size() ) {
+				state = new MultiState( true );
+			} else {
+				Object[] fileList = Arrays.copyOfRange( list.toArray(), index, index + this.count );
+				// [{"state": "SUCCESS","url": "/upload/image/20141105/1415179533898026714.jpg"}]
+				// {"state": "SUCCESS","total": 2,"start": 0, 
+				// 	list: 
+				//		[{"state": "SUCCESS","url": "/upload/image/20141105/1415179533898026714.jpg"},
+				//		{"state": "SUCCESS","url": "/upload/image/20141109/1415536719589040616.jpg"} ]}
+				state = this.getState( fileList );
+			}
+			
+			state.putInfo( "start", index );
+			state.putInfo( "total", list.size() );
 		}
-		
-		
-		if ( index < 0 || index > list.size() ) {
-			state = new MultiState( true );
-		} else {
-			Object[] fileList = Arrays.copyOfRange( list.toArray(), index, index + this.count );
-			state = this.getState( fileList );
-		}
-		
-		state.putInfo( "start", index );
-		state.putInfo( "total", list.size() );
-		
-		// 处理连接问题 
-		
+
 		return state;
 		
 	}
@@ -86,6 +113,28 @@ public class FileManager {
 			file = (File)obj;
 			fileState = new BaseState( true );
 			fileState.putInfo( "url", PathFormat.format( this.getPath( file ) ) );
+			state.addState( fileState );
+		}
+		
+		return state;
+		
+	}
+	
+	// 处理ailiyun数据 
+	private State getOSSState ( Object[] files ) {
+		
+		MultiState state = new MultiState( true );
+		BaseState fileState = null;
+		
+//		File file = null;
+		
+		for ( Object obj : files ) {
+			if ( obj == null ) {
+				break;
+			}
+//			file = (File)obj;
+			fileState = new BaseState( true );
+			fileState.putInfo( "url", PathFormat.format( OSSClientProperties.endPoint + obj ) );
 			state.addState( fileState );
 		}
 		
