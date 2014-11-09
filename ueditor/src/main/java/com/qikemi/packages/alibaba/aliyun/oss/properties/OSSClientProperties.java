de4cf31cdfdf0f2c2eb2520871833e5c7eb85a71
xiexianbin <a.b@hotmail.com>
2014-11-09 22:24:33 +0800

    update

--- ueditor/src/main/java/com/qikemi/packages/alibaba/aliyun/oss/properties/OSSClientProperties.java
+++ ueditor/src/main/java/com/qikemi/packages/alibaba/aliyun/oss/properties/OSSClientProperties.java
@@ -4,35 +4,38 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 import com.qikemi.packages.utils.SystemUtil;
 
 public class OSSClientProperties {
 
-    private static Properties OSSKeyProperties = new Properties();
-    // 阿里云是否启用配置
-    public static String bucketName = "";
-    public static boolean useStatus = false;
-    public static String key = "";
-    public static String secret = "";
-    public static String endPoint = "";
-    public static String projectName = "";
+	private static Logger logger = Logger.getLogger(OSSClientProperties.class);
+	
+	private static Properties OSSKeyProperties = new Properties();
+	// 阿里云是否启用配置
+	public static String bucketName = "";
+	public static boolean useStatus = false;
+	public static String key = "";
+	public static String secret = "";
+	public static String endPoint = "";
 
-    static {
-	String OSSKeyPath = SystemUtil.getProjectClassesPath() + "OSSKey.properties";
-	// 生成文件输入流
-	FileInputStream inpf = null;
-	try {
-	    inpf = new FileInputStream(new File(OSSKeyPath));
-	    OSSKeyProperties.load(inpf);
-	    useStatus = "true".equalsIgnoreCase((String) OSSKeyProperties.get("useStatus")) ? true : false;
-	    bucketName = (String) OSSKeyProperties.get("bucketName");
-	    key = (String) OSSKeyProperties.get("key");
-	    secret = (String) OSSKeyProperties.get("secret");
-	    endPoint = (String) OSSKeyProperties.get("endPoint");
-	    projectName = (String) OSSKeyProperties.get("projectName");
-	} catch (Exception e) {
-	    e.printStackTrace();
+	static {
+		String OSSKeyPath = SystemUtil.getProjectClassesPath()
+				+ "OSSKey.properties";
+		// 生成文件输入流
+		FileInputStream inpf = null;
+		try {
+			inpf = new FileInputStream(new File(OSSKeyPath));
+			OSSKeyProperties.load(inpf);
+			useStatus = "true".equalsIgnoreCase((String) OSSKeyProperties.get("useStatus")) ? true : false;
+			bucketName = (String) OSSKeyProperties.get("bucketName");
+			key = (String) OSSKeyProperties.get("key");
+			secret = (String) OSSKeyProperties.get("secret");
+			endPoint = (String) OSSKeyProperties.get("endPoint");
+		} catch (Exception e) {
+			logger.warn("系统未找到指定文件：OSSKey.properties --> 系统按照ueditor默认配置执行。");
+		}
 	}
-    }
 
 }
