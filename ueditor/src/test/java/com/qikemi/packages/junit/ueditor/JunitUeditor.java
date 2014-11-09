e8186a46ad421113aaab4dbb09a94ee85c1cd601
xiexianbin <a.b@hotmail.com>
2014-11-09 22:32:28 +0800

    update

--- ueditor/src/test/java/com/qikemi/packages/junit/ueditor/JunitUeditor.java
+++ ueditor/src/test/java/com/qikemi/packages/junit/ueditor/JunitUeditor.java
@@ -7,26 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.qikemi.packages.utils.SystemUtil;
+
 public class JunitUeditor extends HttpServlet {
-    private static final long serialVersionUID = -4500613200275499491L;
+	private static final long serialVersionUID = -4500613200275499491L;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-	    throws ServletException, IOException {
-	System.out.println("doGet");
-	
-	
-	
-//	super.doGet(req, resp);
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("doGet");
+		System.out.println(SystemUtil.getProjectName());
+		// super.doGet(req, resp);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-	    throws ServletException, IOException {
-	System.out.println("doPost");
-	req.setCharacterEncoding("UTF-8");
-	System.out.println(req.getParameter("myEditors"));
-	
-//	super.doPost(req, resp);
-    }
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("doPost");
+		req.setCharacterEncoding("UTF-8");
+		System.out.println(req.getParameter("myEditors"));
+
+		// super.doPost(req, resp);
+	}
 }
